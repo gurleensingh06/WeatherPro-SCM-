d338cfd1b5012c6e0a0bb23671e27705012442c8
Gurnoor <gurnoor0305.becse24@chitkara.edu.in>
2025-05-05 10:29:42 +0530

    added api related code

---
+++ weatherpro.js
@@ -0,0 +1,8 @@
+const loadingElement = document.getElementById('loading');
+        const errorElement = document.getElementById('error-message');
+        const errorTextElement = document.getElementById('error-text');
+        const searchInput = document.getElementById('search-input');
+        const searchButton = document.getElementById('search-button');
+        const getLocationButton = document.getElementById('get-location');
+        const darkModeToggle = document.getElementById('dark-mode-toggle');
+        const toastContainer = document.getElementById('toast-container');
